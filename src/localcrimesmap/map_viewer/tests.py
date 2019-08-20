71a107b400f4bbd43f8f849e43c96c7849f0d53a
Callum Houghton <Callum.Houghton13@hotmail.co.uk>
2019-08-20 14:58:30 +0100

    added map_viewer unit tests.

--- src/localcrimesmap/map_viewer/tests.py
+++ src/localcrimesmap/map_viewer/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class CallsTest(TestCase):
+    def test_map_call(self):
+        response = self.client.get('', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'map_viewer/map.html')
