304364eb64d8a6546005f25a096ff04be487ef52
Callum Houghton <Callum.Houghton13@hotmail.co.uk>
2019-08-21 13:02:22 +0100

    Fixed settings.py comp error

--- src/localcrimesmap/localcrimesmap/settings.py
+++ src/localcrimesmap/localcrimesmap/settings.py
@@ -30,9 +30,8 @@ if os.name == 'nt':
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Heroku Settings
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 
-'django.contrib.gis.db.backends.postgis'
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
