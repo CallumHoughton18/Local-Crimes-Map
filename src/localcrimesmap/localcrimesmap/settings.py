13f7018947bad1b5d4d127a6bb585394a7470213
Callum Houghton <Callum.Houghton13@hotmail.co.uk>
2019-08-21 13:23:39 +0100

    COrrected hostname

--- src/localcrimesmap/localcrimesmap/settings.py
+++ src/localcrimesmap/localcrimesmap/settings.py
@@ -93,7 +93,7 @@ SECRET_KEY =  os.getenv('SECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localcrimesmap.heroku.app.com']
+ALLOWED_HOSTS = ['localcrimesmap.herokuapp.com']
 
 
 # Application definition
