b161aba92e9e7f116b8729cb0350096fd0628d8f
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2019-09-16 17:46:06 +0100

    Added bootbox library and changed confirm dialog popup to use bootbox confirm dialog.

--- src/localcrimesmap/static/js/streetview_open.js
+++ src/localcrimesmap/static/js/streetview_open.js
@@ -1,10 +1,14 @@
+// this script relies on bootbox being loaded as a static file already in the DOM
+
 var openStreetViewModule = (function(){
-    
+
     function OpenStreetView(location){
-        if (confirm("Open crime location in Google StreetView?")){
-            let streetViewURL = "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint="+location;
-            window.open(streetViewURL);
-        }
+        bootbox.confirm("Open crime location in Google StreetView?", function(result){
+            if (result){
+                let streetViewURL = "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint="+location;
+                window.open(streetViewURL);
+            }
+        });
     }
 
     return {
